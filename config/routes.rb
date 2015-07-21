34df8044a2bfa0b24f236b11344699cc83830df1
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-21 17:08:32 -0400

    Set Initial Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
- 
- 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" }
+
+  resources :users do
+    resources :connections
+    resources :projects
+    resources :feedbacks
+    resources :partnerships
+  end
+
+  resources :projects
+  
   root to: "home#index"
 end

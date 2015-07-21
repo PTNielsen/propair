af2ced7e9a25a9b61796d317358ad9368ce19dd4
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-21 17:26:58 -0400

    Revised Model Relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
   validates_presence_of :email, :password, :user_name, :first_name, :last_name, :admin
   validates_uniqueness_of :email, :user_name
   
-  has_many :projects
-  has_many :partnerships, through: :projects
-  has_many :feedbacks, through: :projects
-
+  has_many :projects, class_name: "Project", foreign_key: :author_id
+  has_many :feedbacks, class_name: "Feedback", foreign_key: :author_id
+  
+  has_many :partnerships
   has_many :connections
 end

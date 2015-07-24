5078068f1ef73475d525131c3c5d95b4e9f46cf4
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-24 17:40:43 -0400

    App Controller Adjustment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   # before_action :authenticate_user!
 
-  check_authorization unless: :devise_controller?
+  # check_authorization unless: :devise_controller?
 
   rescue_from CanCan::AccessDenied do
     redirect_to :back, notice: "You are not authorized to see this page"

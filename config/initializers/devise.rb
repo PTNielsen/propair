cc1d246fe811930981f1f18ab37f33263f4ad8ff
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-27 17:24:21 -0400

    Revision to Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ module Devise
       def authenticate!
         token = AuthToken.find_by_key @key
         if token.try :active?
-          success! token.user
+          success! sign_in token.user
         else
           fail! "Invalid auth token"
         end

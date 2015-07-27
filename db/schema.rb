66b932d0235a85b686d0879c3d0a08dc0623fadf
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-27 16:24:23 -0400

    Token Table Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150723222729) do
+ActiveRecord::Schema.define(version: 20150727201808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "auth_tokens", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "key"
+    t.datetime "expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "auth_tokens", ["key"], name: "index_auth_tokens_on_key", using: :btree
+  add_index "auth_tokens", ["user_id"], name: "index_auth_tokens_on_user_id", using: :btree
 
   create_table "connections", force: :cascade do |t|
     t.integer  "user_id"
@@ -89,4 +100,5 @@ ActiveRecord::Schema.define(version: 20150723222729) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "auth_tokens", "users"
 end

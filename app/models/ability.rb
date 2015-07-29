7965c200d288cefc346d035897b2657899cd6d79
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-29 10:18:29 -0400

    Add Request Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,9 +6,11 @@ class Ability
 
     if user.admin?
       can :manage, :all
+      can :partner_request, Project
     else
       can :read, :all
       can :create, Project
+      can :partner_request, Project
     end
 
     can [:update, :destroy], Project, author_id: user.id

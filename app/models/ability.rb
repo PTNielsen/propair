1403109e65eb1b83d29073e2c3db9cbc00fb129c
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-29 10:23:11 -0400

    Skip Authorization on Partner Request

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,11 +6,9 @@ class Ability
 
     if user.admin?
       can :manage, :all
-      can :partner_request, Project
     else
       can :read, :all
       can :create, Project
-      can :partner_request, Project
     end
 
     can [:update, :destroy], Project, author_id: user.id

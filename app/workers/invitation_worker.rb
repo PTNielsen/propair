6257337d65aeee61c1d4818a36562ca4b96850b0
Patrick Nielsen <ptnielsen55@gmail.com>
2015-08-04 11:09:56 -0400

    Invitation Worker

--- app/workers/invitation_worker.rb
+++ app/workers/invitation_worker.rb
@@ -1,8 +1,8 @@
-# class InvitationWorker
-#   include Sidekiq::Worker
+class InvitationWorker
+  include Sidekiq::Worker
 
-#   def perform(invitation_params)
-#     @invitation = Invitation.new(invitation_params)
-#     @invitation.perform
-#   end
-# end
+  def perform(invitation_params)
+    @invitation = Invitation.new(invitation_params)
+    @invitation.perform
+  end
+end
